f01227ab15c95c3188f15d8ce693336435468094
Pheterson Ferry Fernando <phetersonnnz@gmail.com>
2023-08-13 15:16:16 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,36 @@
 module.exports = {
-  content: ['index.html'],
-  darkMode: 'class',
+  content: ["profil.html"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
       padding: "16px",
+    },
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+      "6xl": "3.75rem",
+      "7xl": "4.5rem",
+      "8xl": "6rem",
+      "9xl": "8rem",
+      xxl: "6.25rem",
     },
     extend: {
       blur: {
         xs: "1px",
       },
       colors: {
-        primary: '#FFCF0F',
-        secondary: '#1397FC',
-        dark: '#0f172a',
+        primary: "#FFCF0F",
+        secondary: "#1397FC",
+        dark: "#0f172a",
       },
       screens: {
-        '2xl': '1320px',
+        "2xl": "1320px",
       },
     },
   },
